2c1d988e89b3b8f414c28107a0ef7671b820475b
Weslley Victor <wvictor07@gmail.com>
2019-08-22 22:31:48 +0100

    redesigned the website, changed nav, images and fonts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-let sideBarIcon = document.querySelector('.sidebar-icon');
-let sideBarMobile = document.querySelector('.mobile-sidebar');
-let close = document.querySelector('.close');
-let link = document.querySelectorAll('.mobile-link');
-sideBarIcon.onclick = function(){
-    sideBarMobile.style.display = "block";
-}
-close.onclick = function(){
-    sideBarMobile.style.display = "none";
+const nav = document.querySelector('.nav')
+const navItems = document.querySelectorAll('.nav-items')
+
+let options = {
+  strings: ["I develop web applications with the purpose of enabling people to use the internet - one of the most impressive human creations - to do great things!"],
+  typeSpeed: 45,
+  cursorChar: "|"
 }
 
-for(let i=0;i<link.length;i++){
-    link[i].onclick = function(){
-        sideBarMobile.style.display = "none";
+let typed = new Typed(".type", options)
+
+
+window.onscroll = function(){
+  let position = window.scrollY
+  if(position != 0){
+    nav.style.backgroundColor = 'rgba(0,0,0,.9)'
+    for(let i=0;i<navItems.length;i++){
+        navItems[i].style.fontSize = '18px'
     }
+
+  }
+  else{
+    nav.style.backgroundColor = 'transparent'
+    for(let i=0;i<navItems.length;i++){
+        navItems[i].style.fontSize = '20px'
+    }
+  }
 }
