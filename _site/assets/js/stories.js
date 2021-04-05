ee39a7b7b9132568f906533636e313d9610b5fd5
Weslley Victor <wvictor07@gmail.com>
2021-04-05 01:38:54 +0100

    added _site/ folder to .gitignore since github already builds the jekyll site

--- _site/assets/js/stories.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.querySelector('.progress-bar')
-window.onscroll = function(){
-  //to get the total height of the document you should use scrollHeight on document and subtract the height of the window!
-  let bodyScroll = document.body.scrollHeight-window.innerHeight
-  let scrollPos = window.scrollY
-  let progress = (scrollPos/bodyScroll)*100
-  progressBar.style.width = progress+'%'
-}
